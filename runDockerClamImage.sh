665f53783b817a8273afe63123b50f2e5a7adaa9
Knut Petter Meen <kp@scalytica.net>
2016-07-01 22:34:09 +0200

    removing redundant script

--- runDockerClamImage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Convenience script to start a docker image with pre-installed clamav...time saver!!!
-eval "$(docker-machine env default)"
-
-docker pull kpmeen/docker-clamav
-
-
-docker run -d -p 3310:3310 kpmeen/docker-clamav
