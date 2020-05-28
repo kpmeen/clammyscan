4d47ef92dc8387414bb1cd860e261070d1a8d6d3
Scala Steward <security@scalytica.net>
2020-05-28 01:08:06 +0000

    Update play-json to 2.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   )
 
   val playVersion      = play.core.PlayVersion.current
-  val playJsonVersion  = "2.8.1"
+  val playJsonVersion  = "2.9.0"
   val akkaVersion      = "2.6.5"
   val slf4jVersion     = "1.7.30"
   val logbackVersion   = "1.2.3"
